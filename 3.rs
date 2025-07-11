281f8f6c002dc02ad86f62481fef1cc289d5c3ee
yallabandidinesh <155515794+yallabandidinesh@users.noreply.github.com>
2025-07-11 10:10:43 +0530

    Create 3.rs

---
+++ 3.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // a. Declare a variable x and store value 1000 in it
+    let mut x = 1000;
+
+    // b. Declare a variable y and store value “Programming” in it
+    let y = "Programming";
+
+    // c. Print the values of x and y
+    println!("x = {}", x);
+    println!("y = {}", y);
+
+    // d. Change the value of x to 1100
+    x = 1100;
+
+    // e. Print the values of x and y again
+    println!("Updated x = {}", x);
+    println!("y = {}", y);  // y remains unchanged
+}
